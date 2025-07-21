3c74dab3a1f5d9d9739664716e30f80e1b18cf0d
Zekiah-A <zekiahamoako@outlook.com>
2025-07-21 19:29:00 +0100

    Unify set and seti, start adding render layers to webgl canvas

--- src/pages/index/board-renderer.js
+++ src/pages/index/board-renderer.js
@@ -5,7 +5,10 @@ export class BoardRenderer {
 	/**@type {HTMLCanvasElement}*/canvas
 	/**@type {WebGL2RenderingContext}*/#gl;
 
+	// We separate into different render layers for mod & debugging purposes
 	/**@type {Uint8Array|null}*/#board = null;
+	/**@type {Uint8Array|null}*/#changes = null;
+	/**@type {Uint8Array|null}*/#socketPixels = null;	
 	/**@type {Uint32Array|null}*/#palette = null;
 	/**@type {number}*/#width = 0;
 	/**@type {number}*/#height = 0;
@@ -18,6 +21,8 @@ export class BoardRenderer {
 	/**@type {WebGLProgram}*/#program
 	/**@type {WebGLVertexArrayObject}*/#vao
 	/**@type {WebGLTexture}*/#boardTex
+	/**@type {WebGLTexture}*/#changesTex
+	/**@type {WebGLTexture}*/#socketPixelsTex
 	/**@type {WebGLTexture}*/#paletteTex
 
 	/**@type {mat4}*/#modelMatrix
@@ -29,6 +34,11 @@ export class BoardRenderer {
 	/**@type {WebGLUniformLocation}*/#boardTexUniformLoc
 	/**@type {WebGLUniformLocation}*/#paletteTexUniformLoc
 
+	// Render layers configuration
+	/**
+	 * @typedef {{texture: WebGLTexture, enabled: boolean, blendMode?: string}} RenderLayer
+	 */
+	/**@type {Array<RenderLayer>}*/#renderLayers = [];
 
 	/**
 	 * 
@@ -140,14 +150,9 @@ export class BoardRenderer {
 		gl.vertexAttribPointer(uvLoc, 2, gl.FLOAT, false, 0, 0);
 
 		// Create image textures
-		const boardArr = new Uint8Array(this.#width * this.#height);
-		const boardTex = this.#boardTex = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, boardTex);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, this.#width, this.#height, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, boardArr);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		this.#boardTex = this.#initialiseBoardTexture();
+		this.#changesTex = this.#initialiseBoardTexture();
+		this.#socketPixelsTex = this.#initialiseBoardTexture();
 
 		const paletteArr = new Uint8Array();
 		const paletteTex = this.#paletteTex = gl.createTexture();
@@ -163,6 +168,9 @@ export class BoardRenderer {
 		this.#boardTexUniformLoc = /**@type {WebGLUniformLocation}*/ (gl.getUniformLocation(program, "u_boardTex"));
 		this.#paletteTexUniformLoc = /**@type {WebGLUniformLocation}*/ (gl.getUniformLocation(program, "u_paletteTex"));
 
+		// Initialize render layers
+		this.#initializeRenderLayers();
+
 		// Initialise matrices
 		const model = this.#modelMatrix = mat4.create();
 		mat4.identity(model);
@@ -172,6 +180,36 @@ export class BoardRenderer {
 		mat4.identity(projection);
 		const mvp = this.#mvpMatrix = mat4.create();
 		mat4.identity(mvp);
+	}
+
+	#initializeRenderLayers() {
+		this.#renderLayers = [
+			{ texture: this.#boardTex, enabled: true },
+			{ texture: this.#changesTex, enabled: true },
+			{ texture: this.#socketPixelsTex, enabled: true }
+		];
+	}
+
+	/**
+	 * Enable or disable a specific render layer
+	 * @param {number} layerIndex - 0: board, 1: changes, 2: socketPixels
+	 * @param {boolean} enabled
+	 */
+	setLayerEnabled(layerIndex, enabled) {
+		if (this.#renderLayers[layerIndex]) {
+			this.#renderLayers[layerIndex].enabled = enabled;
+			this.queueRedraw();
+		}
+	}
+
+	/**
+	 * Add a new render layer
+	 * @param {WebGLTexture} texture
+	 * @param {boolean} enabled
+	 */
+	addRenderLayer(texture, enabled = true) {
+		this.#renderLayers.push({ texture, enabled });
+		this.queueRedraw();
 	}
 
 	/**
@@ -242,30 +280,94 @@ export class BoardRenderer {
 	}
 
 	/**
-	 * 
-	 * @param {Uint8Array} board 
+	 * @param {Uint8Array} boardArr Board (canvas), changes, or socket pixels array
+	 */
+	#initialiseBoardTexture(boardArr = new Uint8Array(this.#width * this.#height)) {
+		const gl = this.#gl;
+		const boardTex = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, boardTex);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, this.#width, this.#height, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, boardArr);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		return boardTex;
+	}
+
+	#setupBlending(blendMode = "normal") {
+		const gl = this.#gl;
+		
+		if (blendMode === "normal") {
+			gl.enable(gl.BLEND);
+			gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+		} else if (blendMode === "additive") {
+			gl.enable(gl.BLEND);
+			gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+		} else {
+			gl.disable(gl.BLEND);
+		}
+	}
+
+	/**
+	 * @param {RenderLayer} layer 
+	 */
+	#renderLayer(layer) {
+		if (!layer.enabled) {
+			return;
+		}
+		
+		const gl = this.#gl;
+		
+		this.#setupBlending(layer.blendMode);
+		
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, layer.texture);
+		gl.uniform1i(this.#boardTexUniformLoc, 0);
+
+		gl.activeTexture(gl.TEXTURE1);
+		gl.bindTexture(gl.TEXTURE_2D, this.#paletteTex);
+		gl.uniform1i(this.#paletteTexUniformLoc, 1);
+
+		gl.drawArrays(gl.TRIANGLES, 0, 6);
+	}
+
+	/**
+	 * @param {Uint8Array} board - Base canvas
+	 * @param {Uint8Array} changes - Server delta changes from base fetched canvas
+	 * @param {Uint8Array} socketPixels - Websocket received pixels layer
 	 * @param {Uint32Array} palette
 	 * @param {number} width
 	 * @param {number} height
 	 */
-	setSources(board, palette, width, height) {
+	setSources(board, changes, socketPixels, palette, width, height) {
 		this.#board = board;
+		this.#changes = changes;
+		this.#socketPixels = socketPixels;
 		this.#palette = palette;
 		this.#width = width;
 		this.#height = height;
 
 		const gl = this.#gl;
+	
 		// Update board tex
 		gl.bindTexture(gl.TEXTURE_2D, this.#boardTex);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, this.#width, this.#height, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, this.#board);
-		
+	
+		// Update changes tex
+		gl.bindTexture(gl.TEXTURE_2D, this.#changesTex);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, this.#width, this.#height, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, this.#changes);
+
+		// Update socket pixels tex
+		gl.bindTexture(gl.TEXTURE_2D, this.#socketPixelsTex);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, this.#width, this.#height, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, this.#socketPixels);
+
 		// Update palette tex
 		const paletteArr = new Uint8Array(this.#palette.buffer);
 		gl.bindTexture(gl.TEXTURE_2D, this.#paletteTex);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8UI, palette.length, 1, 0, gl.RGBA_INTEGER, gl.UNSIGNED_BYTE, paletteArr);
 		this.queueRedraw();
 	}
-	
+
 	/**
 	 * @param {number} x
 	 * @param {number} y
@@ -289,24 +391,19 @@ export class BoardRenderer {
 			gl.clearColor(0.0, 0.0, 0.0, 0.0);
 			gl.enable(gl.DEPTH_TEST);
 			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
+
 			// Update matrices
 			this.#updateMatrices();
-	
+
 			gl.useProgram(this.#program);
 			gl.bindVertexArray(this.#vao);
 			gl.uniformMatrix4fv(this.#mvpUniformLoc, false, this.#mvpMatrix);
-	
-			gl.activeTexture(gl.TEXTURE0);
-			gl.bindTexture(gl.TEXTURE_2D, this.#boardTex);
-			gl.uniform1i(this.#boardTexUniformLoc, 0);
-	
-			gl.activeTexture(gl.TEXTURE1);
-			gl.bindTexture(gl.TEXTURE_2D, this.#paletteTex);
-			gl.uniform1i(this.#paletteTexUniformLoc, 1);
-	
-			gl.drawArrays(gl.TRIANGLES, 0, 6);
-			
+
+			// Render all enabled layers in order
+			for (const layer of this.#renderLayers) {
+				this.#renderLayer(layer);
+			}
+
 			const error = gl.getError();
 			if (error !== gl.NO_ERROR) {
 				console.error("WebGL Error:", error);
