cbd21fe776555f6ac7fbeca20c02d0492f31c2b8
BlobTheKat <64211737+BlobTheKat@users.noreply.github.com>
2025-08-22 09:57:38 +0100

    Create priv.js

---
+++ priv.js
@@ -0,0 +1,8 @@
+science.checked = localStorage.science != '0'
+indicator.textContent = science.checked ? 'ENABLED' : 'DISABLED'
+indicator.style.color = science.checked ? 'green' : 'red'
+science.onchange = () => {
+  localStorage.science = science.checked ? '' : '0'
+  indicator.textContent = science.checked ? 'ENABLED' : 'DISABLED'
+  indicator.style.color = science.checked ? 'green' : 'red'
+}
